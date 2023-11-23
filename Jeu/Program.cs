01af3d635ec2e901032b2ade52f5982fd81cc2f3
leajourdan <ljourdan002@ensc.fr>
2023-11-23 11:20:02 +0100

    ajout fonction case vide

--- Jeu/Program.cs
+++ Jeu/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+int CountZeros(int[] tableau)
+{
+    int count = 0;
+    foreach (int element  in tableau)
+    {
+
+        if (element == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int Lignes(int [][] tableauJeu){
+    int n= tableauJeu.Length;
+    Random indice= new Random();
+    int trouve=0;
+    int k=0;
+    while (trouve==0 & k<n){
+        int a=indice.Next(0,n);
+        if(CountZeros(tableauJeu[a])!=0){
+            trouve=1;
+            return a;
+        }
+        else
+        {
+            k++;
+        }
+    
+    }
+    return -1;     
+}   
+
+int [][] ApresTour(int[][] tableauJeu){
+    int indice=Lignes(tableauDeTableaux);
+    int n= tableauJeu.Length;
+    Random aleatoire= new Random();
+    int nb= aleatoire.Next(0,n);
+    if (indice==-1){
+        Console.WriteLine("Tu as perdu! Gros nul");
+    
+    }
+    else{
+        do {
+            nb= aleatoire.Next(0,n);
+        }
+        while(tableauJeu[indice][nb]!=0);
+    }
+    tableauJeu[indice][nb]=1;
+    return tableauJeu;
+}
+static void AfficherTableauDeTableaux(int[][] tableau)
+    {
+        foreach (var row in tableau)
+        {
+            foreach (var element in row)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+AfficherTableauDeTableaux(ApresTour(tableauDeTableaux));
