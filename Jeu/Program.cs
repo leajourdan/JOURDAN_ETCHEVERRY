30677d69e3c49adb6ee247be38b1118599a345fb
metchever <metchever015@ensc.fr>
2023-11-23 11:54:17 +0100

    AfficherItem et Score

--- Jeu/Program.cs
+++ Jeu/Program.cs
@@ -34,12 +34,12 @@ int Lignes(int [][] tableauJeu){
 }   
 
 int [][] ApresTour(int[][] tableauJeu){
-    int indice=Lignes(tableauDeTableaux);
+    int indice=Lignes(tableauJeu);
     int n= tableauJeu.Length;
     Random aleatoire= new Random();
     int nb= aleatoire.Next(0,n);
     if (indice==-1){
-        Console.WriteLine("Tu as perdu! Gros nul");
+        Console.WriteLine("Tu as perdu! Gros nul");     //Si la grille est pleine , le joueur a perdu
     
     }
     else{
@@ -51,4 +51,34 @@ int [][] ApresTour(int[][] tableauJeu){
     tableauJeu[indice][nb]=1;
     return tableauJeu;
 }
-coucou
+
+void AfficherItem(int numero){
+    if (numero==0)
+        Console.WriteLine("");
+    if (numero==1)
+        Console.WriteLine("¤");
+    if (numero==2)
+        Console.WriteLine("@");
+    if (numero==3)
+        Console.WriteLine("o");
+    if (numero==4)
+        Console.WriteLine("J");
+}
+
+int Score(int [][] tableauJeu){
+    int score=0;
+    int n= tableauJeu.Length;
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            if (tableauJeu[i][j]==1)
+                score+=1;
+            if (tableauJeu[i][j]==2)
+                score+=3;
+            if (tableauJeu[i][j]==3)
+                score+=7;
+            if (tableauJeu[i][j]==4)
+                score+=15;
+        }
+    }
+    return score;
+}
