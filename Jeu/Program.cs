f11abea4c521373819db6cf258c9448b7cd1bdc5
metchever <metchever015@ensc.fr>
2023-11-23 10:42:52 +0100

    Fonctions Lignes v1

--- Jeu/Program.cs
+++ Jeu/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+int CountZeros(int[] tableau)
+{
+    int count = 0;
+    foreach (int element  in tableau)
+    {
+
+        if (element == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int Lignes(int [][] tableauJeu){
+    int n= tableauJeu.Length;
+    Random indice= new Random();
+    int trouve=0;
+    int k=0;
+    while (trouve==0 & k<n){
+        int a=indice.Next(0,n);
+        if(CountZeros(tableauJeu[a])!=0){
+            trouve=1;
+            return a;
+        }
+        else
+        {
+            k++;
+        }
+    
+    }
+    return -1;     
+}   
