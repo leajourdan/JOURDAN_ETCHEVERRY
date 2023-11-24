a1a9cbc3c64683433d194b1195e5805b8a66ac07
leajourdan <ljourdan002@ensc.fr>
2023-11-24 12:15:01 +0100

    Mouvement

--- Jeu/Program.cs
+++ Jeu/Program.cs
@@ -114,3 +114,112 @@ void AfficherTableau(int[][] tableauJeu){
 Console.Write('\n');
 Console.BackgroundColor = ConsoleColor.Black;
 }   
+ 
+
+
+        // Déclaration et initialisation d'un tableau de tableaux
+int[][] tableauDeTableaux = new int[3][];
+
+        // Initialisation des sous-tableaux
+        tableauDeTableaux[0] = new int[] {1,0,0};
+        tableauDeTableaux[1] = new int[] {0,1,2};
+        tableauDeTableaux[2] = new int[] {1,1,2};
+
+int [][] Mouvement (int [][] tableauJeu, char direction){
+    int n=tableauJeu.Length;
+
+   if (direction == 'z'){
+    for (int i=0; i<n ; i++){
+        int fusion=0;
+        for (int j=1; j<n;j++){
+            int k=0;
+            while ( j-1-k>=0 && tableauJeu[j-1-k][i]==0){
+                tableauJeu[j-1-k][i]=tableauJeu[j-k][i];
+                tableauJeu[j-k][i]=0;
+                k++;
+            }
+            if (j-k!=0){
+                if (tableauJeu[j-1-k][i]==tableauJeu[j-k][i] && fusion == 0){
+                    tableauJeu[j-1-k][i]++;
+                    tableauJeu[j-k][i]=0;
+                    fusion=1;
+            }   }
+        }
+    }
+    }
+    if (direction == 'q'){
+        for (int i=0; i<n ; i++){
+            int fusion=0;
+            for (int j=1;j<n;j++){
+                int k=0;
+                while( j-1-k>=0 &&tableauJeu[i][j-1-k]==0 ){
+                    tableauJeu[i][j-1-k]=tableauJeu[i][j-k];
+                    tableauJeu[i][j-k]=0;
+                    k++;
+                }
+                if (j-k!=0){
+                    if (tableauJeu[i][j-1-k]==tableauJeu[i][j-k] && fusion==0){
+                        tableauJeu[i][j-1-k]++;
+                        fusion=1;
+                        tableauJeu[i][j-k]=0;
+                }}
+            }
+        }
+    }
+    
+ if (direction == 'd'){
+        for (int i=0; i<n ; i++){
+            int fusion=0;
+            for (int j=n-2;j>=0;j--){
+                int k=0;
+                while( j+1+k<=n-1 &&tableauJeu[i][j+1+k]==0 ){
+                    tableauJeu[i][j+1+k]=tableauJeu[i][j+k];
+                    tableauJeu[i][j+k]=0;
+                    k++;
+                }
+                if (j+k!=n-1){
+                    if (tableauJeu[i][j+1+k]==tableauJeu[i][j+k] && fusion==0){
+                        tableauJeu[i][j+1+k]++;
+                        fusion=1;
+                        tableauJeu[i][j+k]=0;
+                }}
+            }
+        }
+    }
+    if (direction == 's'){
+        for (int i=0; i<n ; i++){
+            int fusion=0;
+            for (int j=n-1;j>=0;j--){
+                int k=0;
+                while( j+1+k<=n-1 &&tableauJeu[j+1+k][i]==0 ){
+                    tableauJeu [j+1+k][i] =tableauJeu[j+k][i];
+                    tableauJeu[j+k][i]=0;
+                    k++;
+                }
+                if (j+k!=n-1){
+                    if (tableauJeu[j+1+k][i]==tableauJeu[j+k][i] && fusion==0){
+                        tableauJeu[j+1+k][i]++;
+                        fusion=1;
+                        tableauJeu[j+k][i]=0;
+                }}
+            }
+        }
+    }
+
+return tableauJeu;
+}
+
+
+ static void AfficherTableauDeTableaux(int[][] tableauDeTableaux)
+    {
+        for (int i = 0; i < tableauDeTableaux.Length; i++)
+        {
+            for (int j = 0; j < tableauDeTableaux[i].Length; j++)
+            {
+                Console.Write(tableauDeTableaux[i][j] + " ");
+            }
+            Console.WriteLine(); // Passer à la ligne après chaque sous-tableau
+        }
+    }
+ int [][] a = Mouvement(tableauDeTableaux,'s');
+AfficherTableauDeTableaux(a);
